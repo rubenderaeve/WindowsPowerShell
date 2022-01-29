7e4b3a47f180ee9aa8f01508d7bf5bde46981673
Ruben De Raeve <ruben.deraeve@marelec.com>
2022-01-29 18:41:04 +0100

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,19 +3,27 @@ New-Alias -Name s -Value Get-GitStatus -Force -Option AllScope
 function Get-GitCommit { & git commit -ev $args }
 New-Alias -Name c -Value Get-GitCommit -Force -Option AllScope
 function Get-GitCommitAmendNoEdit { & git commit -v --amend --no-edit $args }
-New-Alias -Name gcam -Value Get-GitCommitAmendNoEdit -Force -Option AllScope
+New-Alias -Name cam -Value Get-GitCommitAmendNoEdit -Force -Option AllScope
 function Get-GitAdd { & git add --all $args }
 New-Alias -Name ga -Value Get-GitAdd -Force -Option AllScope
 function Get-GitTree { & git log --graph --oneline --decorate $args }
 New-Alias -Name t -Value Get-GitTree -Force -Option AllScope
 function Get-GitPush { & git push $args }
-New-Alias -Name gps -Value Get-GitPush -Force -Option AllScope
+New-Alias -Name ps -Value Get-GitPush -Force -Option AllScope
+function Get-GitPushForce { & git push -f $args }
+New-Alias -Name psf -Value Get-GitPush -Force -Option AllScope
+function Get-GitPushOrigin { & git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD) $args }
+New-Alias -Name pso -Value Get-GitPushOrigin -Force -Option AllScope
 function Get-GitPull { & git pull $args }
-New-Alias -Name gpl -Value Get-GitPull -Force -Option AllScope
+New-Alias -Name pl -Value Get-GitPull -Force -Option AllScope
 function Get-GitFetch { & git fetch $args }
 New-Alias -Name f -Value Get-GitFetch -Force -Option AllScope
 function Get-GitCheckout { & git checkout $args }
 New-Alias -Name co -Value Get-GitCheckout -Force -Option AllScope
+function Get-GitCheckoutDevelop { & git checkout develop $args }
+New-Alias -Name cod -Value Get-GitCheckoutDevelop -Force -Option AllScope
+function Get-GitCheckoutBranch { & git checkout -b $args }
+New-Alias -Name cob -Value Get-GitCheckoutBranch -Force -Option AllScope
 function Get-GitBranch { & git branch $args }
 New-Alias -Name b -Value Get-GitBranch -Force -Option AllScope
 function Get-GitBranchAll { & git branch --all $args }
@@ -29,15 +37,17 @@ New-Alias -Name gd -Value Get-GitDiff -Force -Option AllScope
 function Get-GitDiffCached { & git diff -M --cached $args }
 New-Alias -Name gdc -Value Get-GitDiffCached -Force -Option AllScope
 function Get-GitRebase { & git rebase $args }
-New-Alias -Name grb -Value Get-GitRebase -Force -Option AllScope
+New-Alias -Name rb -Value Get-GitRebase -Force -Option AllScope
 function Get-GitRebaseContinue { & git rebase --continue $args }
-New-Alias -Name grbc -Value Get-GitRebaseContinue -Force -Option AllScope
+New-Alias -Name rbc -Value Get-GitRebaseContinue -Force -Option AllScope
 function Get-GitRebaseInteractive { & git rebase -i $args }
-New-Alias -Name grbi -Value Get-GitRebaseInteractive -Force -Option AllScope
+New-Alias -Name rbi -Value Get-GitRebaseInteractive -Force -Option AllScope
+function Get-GitRebaseOriginDevelop { & git rebase origin/develop $args }
+New-Alias -Name rbod -Value Get-GitRebaseOriginDevelop -Force -Option AllScope
 function Get-GitStash { & git stash $args }
-New-Alias -Name gst -Value Get-GitStash -Force -Option AllScope
+New-Alias -Name st -Value Get-GitStash -Force -Option AllScope
 function Get-GitStashPop { & git stash pop $args }
-New-Alias -Name gstp -Value Get-GitStashPop -Force -Option AllScope
+New-Alias -Name stp -Value Get-GitStashPop -Force -Option AllScope
 function Get-GitReset { & git reset $args }
 New-Alias -Name gr -Value Get-GitReset -Force -Option AllScope
 function Get-GitResetPatch { & git reset --patch $args }
@@ -46,3 +56,5 @@ function Get-GitResetHard { & git reset --hard $args }
 New-Alias -Name grh -Value Get-GitResetHard -Force -Option AllScope
 function Get-GitResetSoftHead { & git reset --soft HEAD~ $args }
 New-Alias -Name grsh -Value Get-GitResetSoftHead -Force -Option AllScope
+function Get-DotnetFormat { & dotnet format $args }
+New-Alias -Name df -Value Get-DotnetFormat -Force -Option AllScope
